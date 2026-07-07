cab7cea83e7d58ea78c5b377d95b622b375a91a6
Georgios Karakatsanis <gkceltics@gmail.com>
2026-07-07 09:13:40 +0200

    created the muenze Enum

---
+++ src/main/java/com/smartcoffee/logic/Muenze.java
@@ -0,0 +1,36 @@
+package com.smartcoffee.logic;
+
+public enum Muenze
+{
+    CENT_1 (1, "1 Ct"),
+    CENT_2 (2, "2 Ct"),
+    CENT_5 (5, "5 Ct"),
+    CENT_10 (10, "10 Ct"),
+    CENT_20 (20, "20 Ct"),
+    CENT_50 (50, "50 Ct"),
+    EURO_1 (100, "1 €"),
+    EURO_2 (200, "2 €");
+
+
+    private final int wertInCents;
+    private final String anzeigeName;
+
+
+    Muenze(int wertInCents, String anzeigeName)
+    {
+        this.wertInCents = wertInCents;
+        this.anzeigeName = anzeigeName;
+    }
+
+    public int getWertInCents()
+    {
+        return wertInCents;
+    }
+
+    public String getAnzeigeName()
+    {
+        return anzeigeName;
+    }
+
+
+}
